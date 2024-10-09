ebedf704136dd76ba1d868eae823939fc9d4a832
SashaDynia <sasaivadim@gmail.com>
2024-10-09 19:57:08 +0100

    yep

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const result= 5 + 5 + '5';
+console.log(result);
+
+const email = "sasaivadim@gmail.com";
+console.log(email.includes('@'));
+console.log(email.length);
+let word1 = 'My ';
+let word2 = 'name ';
+let word3 = 'is';
+fullName = word1 + word2 + word3 + ' Viktor';
+console.log(fullName);
+
+let userName = 'Олександр';
+let payment = 1200;
+console.log(alert(`Дякуємо, ${userName}е! До сплати ${payment} гривень`))
